b887f7352a0d999507c3433cf3fbfb8926f19bff
DragooonDEV <37697754+DragooonDEV@users.noreply.github.com>
2018-07-31 20:56:07 -0400

    Add files via upload

--- application/views/crud.php
+++ application/views/crud.php
@@ -2,27 +2,48 @@
 <html LANG="es">
 <head>
     <title>CRUD Prueba </title>
-    <link href="<?php echo base_url('public/css/bootstrap.css')?>" rel="stylesheet">
+    
+    <link href="<?php  echo base_url('public/css/bootstrap.css')?>" rel="stylesheet">
+    <script src="<?php echo base_url('public/js/jquery.min.js')?>"> </script>
     <script src="<?php echo base_url('public/js/bootstrap.js')?>"> </script>
+
+    
+    
 </head>
 
 <body>
 
-    <header>
-    cabeza
-    <header>
+
     <div id="container">
 
-    <div class="col-md-5">
+    <div class="col-md-12">
+    
 
+    <div class="row">
+    <div class="col-md-12">
     <?php
     $this->load->view($contenido);
     ?>
+   
+   
     </div>
+    
+    </div>
+
+   
     </div>
     <footer>
+    <hr>
+    <center>  <a href="<?php echo base_url(); ?>cexcel/dExcel">Exportar Excel</a>  </center>
+    <center> Copyright <-- Trabajo para solutoria </center>
+
+    </hr>
     
     </footer>
+
+
+    </div>
+
 
 
 </body>
