d392ae168fdb742a06e4a43d01ac634bb59cae8d
DragooonDEV <37697754+DragooonDEV@users.noreply.github.com>
2018-07-31 20:55:28 -0400

    Add files via upload

--- application/models/Model_Usuario.php
+++ application/models/Model_Usuario.php
@@ -13,4 +13,59 @@ class Model_Usuario extends CI_Model {
         $query = $this->db->query("select *  from perfil");
         return $query->result();
     }
+
+public function insertUsuario($idper, $nombres, $apellidos, $correo, $telefono){
+
+$arrayDatos = array(
+    'per_id' => $idper,
+    'usu_nombres' => $nombres,
+    'usu_apellidos' => $apellidos,
+    'usu_correo' => $correo,
+    'usu_telefono' => $telefono
+);
+
+$this->db->insert('usuario', $arrayDatos);
+
+}
+
+public function listaUsuario(){
+    $query = $this->db->query("SELECT * FROM USUARIO u inner join perfil p on u.per_id = p.per_id ");
+    return $query->result();
+}
+
+public function deleteUsuario($id){
+    $this->db->where('usu_id', $id);
+    $this->db->delete('usuario');
+}
+
+public function editUsuario($id){
+ $consulta  = $this->db->query("SELECT * FROM USUARIO u inner join perfil p on u.per_id = p.per_id WHERE u.usu_id=$id");
+return $consulta->result();
+}
+
+public function updateUsuario($txtUsuid, $txtPerid, $txtNombres, $txtApellidos, $txtCorreo, $txtTeléfono){
+
+$array = array(
+
+    'per_id' => $txtPerid,
+    'usu_nombres' => $txtNombres,
+    'usu_apellidos' => $txtApellidos,
+    'usu_correo' => $txtCorreo,
+    'usu_telefono' => $txtTeléfono
+
+);
+$this->db->where('usu_id',$txtUsuid );
+$this->db->update('usuario', $array);
+
+}
+
+public function getUsuario(){
+    $this->db->select('*');
+    $this->db->from('usuario');
+    $query = $this->db->get();
+                
+    return $query;
+}
+
+
 }
